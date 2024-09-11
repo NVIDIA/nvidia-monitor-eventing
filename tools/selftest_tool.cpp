bb8766c576ebc3259e8b3fe338e2a059a8c9534b
pbajaj <pbajaj@nvidia.com>
2024-09-11 16:31:55 +0530

    Reverting #include order Fixes JIRA https://

--- tools/selftest_tool.cpp
+++ tools/selftest_tool.cpp
@@ -8,8 +8,8 @@
  * license agreement from NVIDIA CORPORATION is strictly prohibited.
  */
 
-#include "cmd_line.hpp"
 #include "common.hpp"
+#include "cmd_line.hpp"
 #include "dat_traverse.hpp"
 #include "dbus_accessor.hpp"
 #include "selftest.hpp"
